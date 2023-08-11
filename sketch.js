db5e33eca10db77006476d1379b3c785a01ec75d
miqueiasvitor14 <141414295+miqueiasvitor14@users.noreply.github.com>
2023-08-11 08:18:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+  ;background('green');
+}
+
+  function draw() {
+
+
+stroke('purple')
+fill('yellow');
+
+    
+  if(mouseIsPressed)
+    rect(mouseX, mouseY,50,30);
+    
+
+
+
+}
